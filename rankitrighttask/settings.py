cc1c817b7a8b4daad32dcb602a98b8b912029f9c
saiprathapreddy <saiprathapreddy777@gmail.com>
2021-04-17 18:09:24 +0530

    whitenoise

--- rankitrighttask/settings.py
+++ rankitrighttask/settings.py
@@ -125,3 +125,9 @@ STATICFILES_DIRS=[
 ]
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+MIDDLEWARE_CLASSES = (
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
