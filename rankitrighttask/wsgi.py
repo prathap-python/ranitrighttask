0d8a36f54b5e0352fa048bfc63ea59b84f5d7f8a
saiprathapreddy <saiprathapreddy777@gmail.com>
2021-04-17 20:29:47 +0530

    parenthesis added

--- rankitrighttask/wsgi.py
+++ rankitrighttask/wsgi.py
@@ -14,4 +14,4 @@ from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rankitrighttask.settings')
 
-application = Cling(get_wsgi_application()
+application = Cling(get_wsgi_application())
